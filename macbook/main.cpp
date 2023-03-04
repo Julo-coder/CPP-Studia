6e7854d7c8047cc179bb8af7fa4301bdf81996eb
Julo-Coder <julokozlowskipriv@gmail.com>
2023-03-04 17:43:29 +0100

    Adding some files from macbook

---
+++ macbook/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int firstNum = 3;
+    int secdNum = 7;
+
+    cout << "First number: " << firstNum << endl;
+    cout << "Secound number: " << secdNum << endl;
+    return 0;
+}
