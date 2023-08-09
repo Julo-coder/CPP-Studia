c01ce73001432fb7c83811029e1d3f29db03fbf9
Julo-Coder <julokozlowskipriv@gmail.com>
2023-08-09 20:13:08 +0200

    Dodanie zadania quick.

---
+++ quick.cpp
@@ -0,0 +1,49 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     // int sum = 0;
+//     // int n;
+//     // cin >> n;
+//     // for(int j = 0; j < 1000 * n; j++){
+//     //     for(int k = n; k > 0; k/=2) sum++;
+//     // }
+
+//     // cout << sum;
+
+//     // unsigned int mask = 1, counter = 0,key = 0x87654321;
+//     // for(int k = 0; k < 32; k++, mask <<= 1)if(key bitand mask)counter++;
+//     // cout << counter;
+
+//     bool search(char tab[], char key, int size){
+
+//     }
+//         if(size < 0){
+//             return false;
+//         }
+//         if(tab[size - 1] == key){
+//             return true;
+//         }
+//         else{
+//             search(tab, key, size - 1);
+//         } 
+//     return 0;
+
+// }
+
+
+// bool search(char tab[], char key, int size){
+//         if(size < 0){
+//             return false;
+//         }
+//         if(tab[size - 1] == key){
+//             return true;
+//         }
+//         else{
+//             search(tab, key, size - 1);
+//         } 
+// }
+
+// int main(){
+//     char table[] = {a, b, d, f,ds,s}
+//     serach
+// }
