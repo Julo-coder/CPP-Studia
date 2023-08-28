1adacdb2429ee6eaa0fcf5e864c46dbec18dab17
Julo-Coder <julokozlowskipriv@gmail.com>
2023-08-28 20:02:22 +0200

    Dodanie pliku do ćwiczenia C++.

---
+++ Poprawka/zad_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double result = 1 + (34 % 7);
+    double eleven = 34 % 11;
+    cout << result << endl ;
+    cout << eleven;
+
+    return 0;
+}
